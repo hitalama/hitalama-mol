b07d89c712a42b30d163d57f7b8fe3b74a788215
stan-donarise <stan.donarise@gmail.com>
2024-10-22 16:04:29 +0300

    app body init

--- app/app.view.css.ts
+++ app/app.view.css.ts
@@ -2,9 +2,8 @@ namespace $.$$ {
 	
 	$mol_style_define( $shm_hitalama_app, {
 		
-		Avatar: {
-			width: '20rem',
-			height: '20rem',
+		Search_block: {
+			gap: $mol_gap.block,
 		},
 		
 	} )
--- app/app.view.ts
+++ app/app.view.ts
@@ -7,8 +7,14 @@ namespace $.$$ {
 			return this.$.$hyoo_crus_glob.home().hall_by( $shm_hitalama_profile, {} )
 		}
 
+		@ $mol_mem
 		token_str() {
 			return this.token()?.Token()?.val() || ''
+		}
+
+		@ $mol_mem
+		body() {
+			return this.token_str() ? super.body() : this.body_noauth()
 		}
 		
 	}
