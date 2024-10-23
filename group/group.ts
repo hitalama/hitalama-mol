88586be0926df28767ff1ccfeae15a30a99583c1
stan-donarise <stan.donarise@gmail.com>
2024-10-23 21:02:09 +0300

    $shm_hitalama_group entity added

---
+++ group/group.ts
@@ -0,0 +1,15 @@
+namespace $ {
+
+	export class $shm_hitalama_group extends $hyoo_crus_entity.with({
+
+		Name: $hyoo_crus_atom_str,
+		Owner_id: $hyoo_crus_atom_str,
+		Photo_url: $hyoo_crus_atom_str,
+		Members_count: $hyoo_crus_atom_real,
+		
+	}){
+		
+	}
+
+	
+}
