1bafca55a46eb66e3688db138790c95594bd37f2
stan-donarise <stan.donarise@gmail.com>
2025-01-26 14:12:37 +0300

    private boards

--- board/catalog/catalog.view.ts
+++ board/catalog/catalog.view.ts
@@ -7,6 +7,14 @@ namespace $.$$ {
 			return $shm_hitalama_profile.current()?.boards(null)!
 		}
 
+		@ $mol_action
+		add() {
+			const entity = this.list()?.make( {'': $hyoo_crus_rank_deny} )
+			entity?.title( this.title_default() )
+			this.spread( entity.ref().description )
+			return entity
+		}
+
 	}
 
 }
